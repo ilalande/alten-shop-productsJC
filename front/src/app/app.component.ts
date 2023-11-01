a931eca6baa01bb417903c505d4b64025ec41561
juliane <jcasier@protonmail.com>
2023-11-01 14:41:15 +0100

    cleaning primeng.module.ts with only used component. Implementing breadcrumb and navBar moving if pinned in app.component.html

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
+import { SidenavService } from 'app/base/sidenav/sidenav.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'front';
+export class AppComponent implements OnInit {
+  constructor(private readonly sidenavService: SidenavService) {}
+
+  get getExpanded(): boolean {
+    return this.sidenavService.getExpanded();
+  }
+  get getPinned(): boolean {
+    return this.sidenavService.getPinned();
+  }
+
+  ngOnInit() {}
 }
