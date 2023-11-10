61400c561d99d833d3683441fe81dfa13213f960
juliane <jcasier@protonmail.com>
2023-11-10 15:35:08 +0100

    implementing delete items. Not working in case of multiple deletes on multiple items. Rest is ok

--- front/src/app/product/product.service.ts
+++ front/src/app/product/product.service.ts
@@ -2,15 +2,24 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/internal/Observable';
 import { Product } from './product.model';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductService {
   private apiBaseUrl = 'http://localhost:5000';
+  error = new Subject<string>();
+
   constructor(private http: HttpClient) {}
 
   getProductList(): Observable<Product[]> {
     return this.http.get<Product[]>(`${this.apiBaseUrl}/products`);
+  }
+
+  deleteProduct(id: number): Observable<null> {
+    console.log(id);
+
+    return this.http.delete<null>(`${this.apiBaseUrl}/products/${id}`);
   }
 }
--- front/src/app/product/products-admin/products-admin.component.ts
+++ front/src/app/product/products-admin/products-admin.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { BaseTableLoader } from 'app/shared/ui/table/base-table-loader.class';
+import { Subscription } from 'rxjs';
 import { ProductService } from '../product.service';
 import { Product } from '../product.model';
 import { CrudItemOptions } from 'app/shared/utils/crud-item-options/crud-item-options.model';
@@ -9,16 +11,55 @@ import { productTableConfig } from './product-admin.config';
   templateUrl: './products-admin.component.html',
   styleUrls: ['./products-admin.component.scss'],
 })
-export class ProductsAdminComponent implements OnInit {
+export class ProductsAdminComponent extends BaseTableLoader implements OnInit {
   producList: Product[] = [];
   public configCrudItemOptions: CrudItemOptions[] = productTableConfig;
   public entity = Product; // class of new entry
-  constructor(private productService: ProductService) {}
+  error = null;
+  private errorSub: Subscription;
+
+  constructor(private productService: ProductService) {
+    super();
+  }
 
   ngOnInit(): void {
-    this.productService.getProductList().subscribe((productsFromJsonFile) => {
-      this.producList = productsFromJsonFile;
-      console.log(this.producList);
+    this.errorSub = this.productService.error.subscribe((errorMessage) => {
+      this.error = errorMessage;
     });
+    this.getProductList();
+  }
+  getProductList() {
+    this.productService.getProductList().subscribe((productsData) => {
+      this.producList = productsData;
+    });
+  }
+
+  deleteProduct(productId: number) {
+    console.log('id of one product' + productId);
+
+    this.productService.deleteProduct(productId).subscribe((data) => {
+      console.log(data);
+      this.getProductList();
+    });
+    // this.handleReload(this.productService.deleteProduct(productId));
+  }
+
+  deleteProducts(productIds: number[]) {
+    let idsToDelete = [...productIds];
+    for (const id of idsToDelete) {
+      this.deleteProduct(id);
+    }
+    idsToDelete = [];
+  }
+  // Problem in case of multiple deletes on multiple items in the same time : old selected datas are not removed.
+  // Tried to fix it with BaseTableLoader class and lazyLoaded. And  but doesn'y work. And tried with handleReload (see comment upside) but doesn'y work either
+
+  onDelete(event: number[]) {
+    if (event.length > 1) {
+      console.log(event);
+      return this.deleteProducts(event);
+    } else {
+      return this.deleteProduct(event[0]);
+    }
   }
 }
