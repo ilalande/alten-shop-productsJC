eef22a1bed8b2e9b97850e244723aeb331526b82
juliane <jcasier@protonmail.com>
2023-11-13 13:09:47 +0100

    correcting details : removing .js in imports, adding ProductModulue in App imports, adding description in config file

--- front/src/app/product/product.module.ts
+++ front/src/app/product/product.module.ts
@@ -3,8 +3,8 @@ import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { ProductsAdminComponent } from './products-admin/products-admin.component';
 import { ProductsComponent } from './products/products.component';
-import { ProductService } from './product.service.js';
-import { SharedModule } from 'app/shared/shared.module.js';
+import { ProductService } from './product.service';
+import { SharedModule } from 'app/shared/shared.module';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
